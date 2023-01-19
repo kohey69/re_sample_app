57e4f3f730825f1606d01be9461012cb2a6c83e8
kohey69 <kohey.chill.out@gmail.com>
2023-01-19 16:51:24 +0900

    ユーザー登録機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])   
+  end
+
+  def create
+    # @user = User.new(params[:user])
+    @user = User.new(user_params)
+    if  @user.save
+      flash[:success] = "ユーザー登録成功"
+      redirect_to @user #redirect_to user_url(@user)と同じ
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 end
+
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
